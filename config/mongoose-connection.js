e2a5d67e67ea4c55147dae9e42c5d3c6f842c356
Chitranjan Rana <ckrana9507@gmail.com>
2026-02-23 16:34:01 +0530

    23 feb

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose');
+const config = require("config");
+const dbgr = require('debug')("development:mongoose");
+
 
 mongoose
-.connect("mongodb://localhost:27017/scatch")
+.connect(`${config.get("MONGODB_URI")}/scatch`)
 .then(function(){
-    console.log("Connected");
+    dbgr("Connected");
 })
 .catch(function(err){
     console.log(err);
