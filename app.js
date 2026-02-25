1f11bc5c1c7692a7adc8df3ec7378b87b13a6384
Chitranjan Rana <ckrana9507@gmail.com>
2026-02-25 18:34:38 +0530

    25feb

--- app.js
+++ app.js
@@ -1,34 +1,43 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cookieParser = require('cookie-parser');
-const path = require('path');
+const cookieParser = require("cookie-parser");
+const path = require("path");
+const session = require("express-session");
+const flash = require("connect-flash");
+require("dotenv").config();
+
+require("./config/mongoose-connection");
+
 const ownersRouter = require("./routes/ownersRouter");
 const usersRouter = require("./routes/usersRouter");
 const productsRouter = require("./routes/productsRouter");
+const indexRouter = require("./index");
 
-
-const db = require("./config/mongoose-connection");
-
-
-// setup things
+// middleware
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+app.use(
+  session({
+    name: "sessionId",
+    secret: String(process.env.SESSION_SECRET),
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(flash());
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 
-
-// Routing
+// routes
+app.use("/", indexRouter);
 app.use("/owners", ownersRouter);
 app.use("/users", usersRouter);
 app.use("/products", productsRouter);
 
-
-
-
 const PORT = 3000;
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-})
-
-
+  console.log(`Server running on http://localhost:${PORT}`);
+});
